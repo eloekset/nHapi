4e2f5d9ca30471514e0684b69e586cb919fb2a94
Eivind Gussiås Løkseth <Eivind.Gussias.Lokseth@hemit.no>
2026-08-19 09:16:48 +0200

    Add tool to create SourceDB for ModelGenerator

---
+++ src/ModelGeneratorSourceDB/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ModelGeneratorSourceDB
+{
+    using System.Reflection.Emit;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = Args.Configuration.Configure<SourceDB>().CreateAndBind(args);
+            command.Execute();
+        }
+    }
+}
---
+++ src/ModelGeneratorSourceDB/SqliteImporter.cs
@@ -0,0 +1,38 @@
+﻿namespace ModelGeneratorSourceDB
+{
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.Extensions.Logging;
+
+    internal class SqliteImporter
+    {
+        private readonly ILogger<SqliteImporter> _logger;
+
+        public SqliteImporter(ILogger<SqliteImporter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void ImportCsvFiles(string directory, string connectionString)
+        {
+            var dir = new DirectoryInfo(directory);
+            if (dir.Exists)
+            {
+                var csvFiles = dir.GetFiles();
+                foreach (var expectedFileName in TableNames.All)
+                {
+                    if (!csvFiles.Where(f => f.Name.Equals(expectedFileName, System.StringComparison.InvariantCultureIgnoreCase)).Any())
+                    {
+                        _logger.LogError($"{expectedFileName}.csv not found");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                _logger.LogWarning($"CSV import folder {directory} does not exist");
+            }
+        }
+    }
+}
